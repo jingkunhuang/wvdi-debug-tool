7b6eb89fb549f57e65cc67bc87873ce8e02d2026
jingkhua <jingkhua@cisco.com>
2024-11-18 01:06:50 +0800

    typo

--- src/logVisualize/index.js
+++ src/logVisualize/index.js
@@ -1,5 +1,5 @@
 import Graph_VDIFramework_Heartbeat from './Graph_VDIFramework_Heartbeat';
-import Graph_VDIFramework_Roundtrip from './Graph_VDIFramework_roundtrip';
+import Graph_VDIFramework_Roundtrip from './Graph_VDIFramework_Roundtrip';
 import Graph_TelephonyStateMachine from './Graph_TelephonyStateMachine';
 
 const log_graphs = [ new Graph_VDIFramework_Heartbeat(), new Graph_VDIFramework_Roundtrip(), new Graph_TelephonyStateMachine() ];
